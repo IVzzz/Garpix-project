a4e93579c05262978e0ea6779942112a14b9b8fa
Denis <denis_kuza2001@mail.ru>
2022-07-07 21:28:05 +0300

    __init__

---
+++ PyScripts/box.py
@@ -0,0 +1,20 @@
+class Box:
+    def __init__(self, groupId: int, width: int, height: int, length: int, boxCount: int, stacking: bool, turnovers: bool, mass: float, position: int):
+        self.groupId = groupId
+        self.width = width
+        self.height = height
+        self.length = length
+        self.boxCount = boxCount
+        self.stacking = stacking
+        self.turnovers = turnovers
+        self.mass = mass
+        self.__position = position
+
+    def setPosition(self, position):
+        self.__position = position
+
+    def getPosition(self):
+        return self.__position
+
+    
+
