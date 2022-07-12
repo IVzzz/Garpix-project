549ab9ee0225aba4d269943d21b6f3b7e3dea71b
Denis <denis_kuza2001@mail.ru>
2022-07-12 20:21:58 +0300

    logging

--- PyScripts/box.py
+++ PyScripts/box.py
@@ -1,5 +1,11 @@
+import logging
+
+
 class Box:
-    def __init__(self, groupId: int, width: int, height: int, length: int, boxCount: int, stacking: bool, turnovers: bool, mass: float, position: int):
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
+
+    def __init__(self, groupId: int, width: int, height: int, length: int, boxCount: int, stacking: bool,
+                 turnovers: bool, mass: float, position: int):
         self.groupId = groupId
         self.width = width
         self.height = height
@@ -8,10 +14,14 @@ class Box:
         self.stacking = stacking
         self.turnovers = turnovers
         self.mass = mass
+        # Position is center of parallelepiped
         self.__position = position
+        logging.info(f'New box: groupId:{groupId} w:{width} h:{height} l:{length} bCount:{boxCount} mass:{mass}')
 
     def setPosition(self, position):
         self.__position = position
 
     def getPosition(self):
         return self.__position
+
+    
---
+++ PyScripts/main.py
@@ -0,0 +1,3 @@
+from PyScripts.box import Box
+
+box = Box(1, 2, 3, 4, 5, True, True, 8, 9)
