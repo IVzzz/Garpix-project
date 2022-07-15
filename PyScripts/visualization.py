b2174ec6e1c751d670b5ebe86c9f0837ffec50a6
Denis <denis_kuza2001@mail.ru>
2022-07-15 23:02:48 +0300

    vizualization of the box

--- PyScripts/visualization.py
+++ PyScripts/visualization.py
@@ -1,7 +1,4 @@
-import logging
-import numpy as np
 from myplot import plot_verticles
-import pymesh
 
 
 from PyScripts.box import Box
@@ -9,4 +6,4 @@ from PyScripts.box import Box
 box = Box(1, 2, 3, 4, 5, 6)
 box.setPosition(5, 5, 5)
 
-plot_verticles(vertices = box.getVertices(), isosurf = False)
+plot_verticles(vertices=box.getVertices(), isosurf=False)
