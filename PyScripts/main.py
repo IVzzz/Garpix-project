52618aec0004f9d900025d000eaa1923b6678567
Denis <denis_kuza2001@mail.ru>
2022-07-16 00:08:32 +0300

    extracting data from json

--- PyScripts/main.py
+++ PyScripts/main.py
@@ -1,3 +1,27 @@
-from PyScripts.box import Box
+import json
+import fragmentation
+import logging
 
-box = Box(1, 2, 3, 4, 5, True, True, 8, 9)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
+
+# Extracting data from json[START]
+filename = r"D:\PyProjects\Garpix-project\Data\_vg_85_bgg5jsons\0\30_cl.json"
+
+with open(filename, encoding="utf8") as file:
+    base = file.read()
+
+jsonString = '{"a":54, "b": 28}'
+
+aDict = json.loads(base)
+
+boxes = [("id", "mass", "size", "count", "group_id")]
+
+for box in aDict["cargo_groups"]:
+    boxes.append((box["id"], box["mass"], box["size"], box["count"], box["group_id"]))
+# Extracting data from json[END]
+
+# Classifying data
+classes = {}
+classes = fragmentation.fragmentationBoxes(boxes)
+
+logging.info(f"Classified data from {filename}: {classes}")
