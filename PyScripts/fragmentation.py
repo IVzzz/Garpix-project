089ea5e87822af28ce2f28b0f168aa31d717bdb2
Solyarich <73170788+Solyarich@users.noreply.github.com>
2022-07-17 20:37:28 +0300

    Add files via upload

--- PyScripts/fragmentation.py
+++ PyScripts/fragmentation.py
@@ -1,13 +1,42 @@
+from box import Box
+
 def fragmentationBoxes(boxes):
 
     classes = {}
 
+    newBoxes = []
 
-    for i in range (1, len(boxes)):
+    for i in range (0, len(boxes)):
         for t in range (3):
-            keyClass = boxes[i][2][t]
-            idBox = boxes[i][0]
+            if (t == 0):
+                keyClass = boxes[i].width
+            elif (t == 1):
+                keyClass = boxes[i].height
+            else:
+                keyClass = boxes[i].length
+
+            idBox = boxes[i].id
             idFlag = False
+            for s in classes.keys():
+                if (keyClass == s):
+                    buffer_ = classes[s]
+                    for k in range (len(buffer_)):
+                        if (buffer_[k][2] == idBox):
+                            idFlag = True
+            if (t == 0 and idFlag == False):
+                buffer = (boxes[i].height, boxes[i].length, boxes[i].id, boxes[i].boxCount)
+                classes.setdefault(keyClass, []).append(buffer)
+                newBoxes.append(Box(boxes[i].id, boxes[i].groupId, boxes[i].width, boxes[i].height, boxes[i].length, boxes[i].boxCount, boxes[i].mass))
+            elif (t == 1 and idFlag == False):
+                buffer = (boxes[i].width, boxes[i].length, boxes[i].id, boxes[i].boxCount)
+                classes.setdefault(keyClass, []).append(buffer)
+                newBoxes.append(Box(boxes[i].id, boxes[i].groupId, boxes[i].height, boxes[i].width, boxes[i].length, boxes[i].boxCount, boxes[i].mass))
+            elif (idFlag == False):
+                buffer = (boxes[i].width, boxes[i].height, boxes[i].id, boxes[i].boxCount)
+                classes.setdefault(keyClass, []).append(buffer)
+                newBoxes.append(Box(boxes[i].id, boxes[i].groupId, boxes[i].length, boxes[i].width, boxes[i].height, boxes[i].boxCount, boxes[i].mass))
+            
+            """
             for s in classes.keys():
                 if (keyClass == s):
                     buffer_ = classes[s]
@@ -23,5 +52,9 @@ def fragmentationBoxes(boxes):
             elif (idFlag == False):
                 buffer = (boxes[i][2][t - 2], boxes[i][2][t - 1], boxes[i][0], boxes[i][3])
                 classes.setdefault(keyClass, []).append(buffer)
+
+"""
     sortedClasses = dict(sorted(classes.items()))
-    return sortedClasses
+
+    print(sortedClasses)
+    return newBoxes
--- PyScripts/main.py
+++ PyScripts/main.py
@@ -1,3 +1,59 @@
-from PyScripts.box import Box
+import json
+import numpy as np
+import fragmentation
+import logging
+from box import Box
 
-box = Box(1, 2, 3, 4, 5, True, True, 8, 9)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S', level=logging.DEBUG)
+
+# Extracting data from json[START]
+filename = r"_vg_85_bgg5jsons\0\30_cl.json"
+
+with open(filename, encoding="utf8") as file:
+    base = file.read()
+
+jsonString = '{"a":54, "b": 28}'
+
+aDict = json.loads(base)
+
+boxes = []
+
+for item in aDict["cargo_groups"]:
+    boxes.append(Box(item['id'], item['group_id'], item['size'][0], item['size'][1], item['size'][2], item['count'], item['mass']))
+# Extracting data from json[END]
+
+# Sorting data by non-growth (quicksort)
+def qsort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        lowArray = []
+        highArray = []
+        equalArray = [] 
+
+        pivot = array[int(len(array) / 2)].length
+        for item in array:
+            if item.length < pivot:
+                lowArray.append(item)
+            elif item.length > pivot:
+                highArray.append(item)
+            else:
+                equalArray.append(item)
+        return qsort(lowArray) + equalArray + qsort(highArray)
+
+
+for item in boxes:
+    logging.info(item.getBoxData())
+
+boxes = qsort(boxes)
+
+for item in boxes:
+    logging.info(item.getBoxData())
+
+fragmentBoxes = fragmentation.fragmentationBoxes(boxes)
+
+fragmentBoxes = qsort(fragmentBoxes)
+
+for item in fragmentBoxes:
+    logging.info(item.getBoxData())
+
